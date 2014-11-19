5e72a639c4896f4377357cad7de092724bf4601c
Martin Strobel <marstr@microsoft.com>
2014-11-19 13:35:02 -0800

    Adding files for NuGet support.

--- CSVUtilities/CSVUtilities/My Project/AssemblyInfo.vb
+++ CSVUtilities/CSVUtilities/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("CSVUtilities")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyTitle("CSVUtilities")>
+<Assembly: AssemblyDescription("Makes easier the process of working with CSV files.")>
+<Assembly: AssemblyCompany("Cloud Platform Tools")>
 <Assembly: AssemblyProduct("CSVUtilities")> 
-<Assembly: AssemblyCopyright("Copyright ©  2014")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("e31ef913-225e-49d0-9a67-01031139291e")> 
+<Assembly: Guid("e31ef913-225e-49d0-9a67-01031139291e")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.1.*")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
