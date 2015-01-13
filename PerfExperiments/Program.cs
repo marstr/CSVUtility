342e0b3e38477c3e30d62506be1319da77fca40d
Martin Strobel <marstr@microsoft.com>
2015-01-13 11:37:24 -0800

    Adding code for unsophisticated perf experiment runner.

--- PerfExperiments/Program.cs
+++ PerfExperiments/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace PerfExperiments
     {
         static void Main(string[] args)
         {
+            var rows = 100000;
+            var cols = 10;
+
+            var totalCells = rows * cols;
+            var tempFile = Path.GetTempFileName();
+
+            var experiments = new[] {
+                new WriteToStreamExperiment(new MemoryStream(), string.Format("Write {0} Cells to Memory", totalCells)),
+                new WriteToStreamExperiment(new FileStream(tempFile,FileMode.Create), string.Format("Write {0} Cells to File", totalCells))};
+                    
+
+            foreach (var runner in experiments)
+            {
+                Console.Write("Starting {0}...", runner.Title);
+                var result = runner.RunExperiment(rows, cols);
+                Console.WriteLine("Finished in {0} seconds.", result.TotalSeconds);                               
+            }            
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
---
+++ PerfExperiments/WriteToStreamExperiment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.CSV;
+
+namespace PerfExperiments
+{
+    public class WriteToStreamExperiment : IPerfExperiment
+    {
+        protected CSVWriter writer;
+
+        public string Title
+        {
+            get
+            {
+                return _Title;
+            }
+        }
+        private readonly string _Title;
+
+        public const string DEFAULT_TITLE = "Stream Writer Experiment";
+
+        public WriteToStreamExperiment(CSVWriter writer, string title = DEFAULT_TITLE)
+        {
+            this.writer = writer;
+            _Title = title;
+        }
+
+        public WriteToStreamExperiment(Stream output, string title = DEFAULT_TITLE)
+        {
+            writer = new CSVWriter(output);
+            _Title = title;
+        }
+
+        public TimeSpan RunExperiment()
+        {
+            return RunExperiment(10000, 10);
+        }
+
+        public TimeSpan RunExperiment(int rows, int columns)
+        {
+            var writeData = new int[columns];
+
+            var r = new Random();
+
+            for(int i = 0; i < columns; i++)
+            {
+                writeData[i] = r.Next();
+            }
+
+            var clock = new Stopwatch();
+            clock.Start();
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    writer.WriteTuple(writeData);
+                }
+            }
+            clock.Stop();
+            return clock.Elapsed;
+        }
+    }
+}
